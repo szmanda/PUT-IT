471af227f7b619797aee45da5a7276259d20c3eb
unknown <michalszmanda.mail@gmail.com>
2021-04-23 13:30:39 +0200

    lab3 - adding to list

--- term2/Programowanie niskopoziomowe w C/lab3/ex4.c
+++ term2/Programowanie niskopoziomowe w C/lab3/ex4.c
@@ -1,36 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct Computer{
-    char *name[32];
-    char *brand[32];
-    float *price;
-};
+typedef struct Computer{
+    char name[32];
+    char brand[32];
+    float price;
+} computer;
 
-struct ArrayOfComputers{
-    struct Computer *comp;
-    struct ArrayOfComputers *next;
-};
+typedef struct ArrayOfComputers arrayOfComputers;
 
-void add(struct ArrayOfComputers *list){
-    struct ArrayOfComputers * last = list;
-    while (last->next != NULL) last = last->next;
-    last->next = (struct ArrayOfComputers *) malloc(sizeof(struct ArrayOfComputers));
-    last->next->comp = (struct Computer *) malloc(sizeof(struct Computer));
-    *last->next->comp->brand = "SpaceX";
-    
-    *last->next->comp->name = "starship";
-    *last->next->comp->price = 2.99f;
-    last->next->next = NULL;
+typedef struct ArrayOfComputers{
+    computer *comp;
+    arrayOfComputers *next;
+} arrayOfComputers;
 
+computer* details(){
+    computer* pc = malloc(sizeof(computer));
+    char name[32], brand[32];
+    float price;
+
+    printf("enter name: ");
+    scanf("%s", pc->name);
+    fflush(stdin);
+    // pc->name = "test";
+    printf("enter brand: ");
+    scanf("%s", pc->brand);
+    fflush(stdin);
+    printf("enter price: ");
+    scanf("%f", &price);
+    pc->price = price;
+
+    return pc;
 }
-void display(struct ArrayOfComputers *list){
-    struct ArrayOfComputers * last = list;
-    while (last->comp != NULL) {printf("name: %s", *last->comp->name); last = last->next;}
+
+void add(arrayOfComputers *list){
+    if (list->comp == NULL){
+        printf("first\n");
+        list->comp = details();
+        list->next = NULL;
+    } else if (list->next == NULL){
+        printf("last\n");
+        list->next = (arrayOfComputers *) malloc(sizeof(arrayOfComputers));
+        list->next->comp = details();
+        list->next->next = NULL;
+    } else {
+        printf("next\n");
+        add(list->next);
+    }
+}
+void display(arrayOfComputers *list){
+    if (list->next == NULL){
+        printf(
+            "Computer %s, %s - %f $",
+            list->comp->brand,
+            list->comp->name,
+            list->comp->price
+        );
+    } else {
+        printf("next\n");
+        add(list->next);
+    }
 }
 int main(){
     char c;
-    struct ArrayOfComputers list;
+    arrayOfComputers list;
+    list.comp = NULL;
+    list.next = NULL;
     for(;;){
         fflush(stdin);
         scanf("%c",&c);
@@ -42,7 +78,7 @@ int main(){
             break;
         else if (c=='R' || c=='q')
             break;
-        else if (c=='D' || c=='q')
+        else if (c=='D' || c=='d')
             display(&list);
     }
     return 0;
