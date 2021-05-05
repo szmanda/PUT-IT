3532c6b412c45c70761f5e2b1b7b8b438b9b2823
unknown <michalszmanda.mail@gmail.com>
2021-05-05 13:02:45 +0200

    finished lab3 (lists)

--- term2/Programowanie niskopoziomowe w C/lab3/ex4.c
+++ term2/Programowanie niskopoziomowe w C/lab3/ex4.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -36,32 +36,67 @@ computer* details(){
 
 void add(arrayOfComputers *list){
     if (list->comp == NULL){
-        printf("first\n");
+        //printf("first\n");
         list->comp = details();
         list->next = NULL;
     } else if (list->next == NULL){
-        printf("last\n");
+        //printf("last\n");
         list->next = (arrayOfComputers *) malloc(sizeof(arrayOfComputers));
         list->next->comp = details();
         list->next->next = NULL;
     } else {
-        printf("next\n");
+        //printf("next\n");
         add(list->next);
     }
 }
 void display(arrayOfComputers *list){
     if (list->next == NULL){
         printf(
-            "Computer %s, %s - %f $",
+            "Computer %s, %s - $%f\n\n",
             list->comp->brand,
             list->comp->name,
             list->comp->price
         );
     } else {
-        printf("next\n");
-        add(list->next);
+        printf(
+            "Computer %s, %s - $%f\n",
+            list->comp->brand,
+            list->comp->name,
+            list->comp->price
+        );
+        display(list->next);
     }
 }
+void rem(arrayOfComputers *list, char name[32]){
+    //printf("remove\n");
+    if (list->next == NULL){
+        if (strcmp(list->comp->name, name) == 0) {
+            list->comp = NULL;
+            printf("removed last computer\n");
+        }
+    } else if (strcmp(list->comp->name, name) == 0) {
+        list->comp = list->next->comp;
+        list->next = list->next->next;
+        printf("removed computer\n");
+    } else {
+        rem(list->next, name);
+    }
+}
+
+void search(arrayOfComputers *list, char name[32]){
+    if (strcmp(list->comp->name, name) == 0)
+    {
+        printf(
+        "Computer was found: %s, %s - $%f\n",
+        list->comp->brand,
+        list->comp->name,
+        list->comp->price
+    );
+    }
+    if (list->next != NULL)
+        search(list->next, name);
+}
+
 int main(){
     char c;
     arrayOfComputers list;
@@ -74,10 +109,20 @@ int main(){
             break;
         else if (c=='N' || c=='n')
             add(&list);
-        else if (c=='S' || c=='s')
-            break;
-        else if (c=='R' || c=='q')
-            break;
+        else if (c=='S' || c=='s') {
+            char name[32];
+            printf("name the pc to search for: ");
+            fflush(stdin);
+            scanf("%s",name);
+            search(&list,name);
+        }
+        else if (c=='R' || c=='r') {
+            char name[32];
+            printf("name the pc to remove: ");
+            fflush(stdin);
+            scanf("%s",name);
+            rem(&list,name);
+        }
         else if (c=='D' || c=='d')
             display(&list);
     }
